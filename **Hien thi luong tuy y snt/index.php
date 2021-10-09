07386f412cc2689234f48f89359123a54a16a85e
nguyenthixuananh <nguyenhixuananh09061999@gmail.com>
2021-10-09 09:13:57 +0700

    hien thi luong tuy y snt

---
+++ **Hien thi luong tuy y snt/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="input">
+    <input type="submit" value="Write">
+</form>
+</body>
+</html>
+<?php
+
+function isPrimeNumber($n)
+{
+    if ($n < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+    $num = $_POST["input"];
+    $count =0;
+    $N =2;
+    for ($i=0; $i<$num; $i++) {
+        while ($count<$num) {
+            if (isPrimeNumber($N)) {
+                echo $N.' ';
+                $count++;
+            } $N++;
+        }
+    }
+}
+?>
+
